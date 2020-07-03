8de214d817c2a07eb23ccd873b182aa529351762
Karina Lazarina <karinyao27@gmail.com>
2020-07-03 20:48:58 +0300

    Initial commit

---
+++ LazarinaKarinaHW1/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './component/app.component';
+import { PostComponent } from './post/post.component';
+import { CommentComponent } from './comment/comment.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent,
+    CommentComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ LazarinaKarinaHW1/src/app/component/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  posts: any[];
+  comments: any[];
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get<any[]>('http://jsonplaceholder.typicode.com/posts')
+      .subscribe(response => this.posts = response);
+
+    this.http
+      .get<any[]>('http://jsonplaceholder.typicode.com/comments')
+      .subscribe(response => this.comments = response);
+  }
+
+}
